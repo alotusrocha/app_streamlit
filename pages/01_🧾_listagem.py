f73d6b4b741e0c0a08e9d61252032eaf5b9700f5
alotusrocha <everton.srocha@gmail.com>
2024-11-02 13:22:02 -0300

    Primeiro Commit2

---
+++ pages/01_🧾_listagem.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from st_aggrid import AgGrid
+
+st.header("Visualização dos Dados")
+
+
+if 'dados' not in st.session_state:
+    st.error("Os dados não foram carregados")
+else:
+    top_n = st.session_state.get('top_n', 10)
+    dados = st.session_state['dados']
+    dados_filtrados = dados.head(top_n)
+    AgGrid(dados_filtrados, fit_columns_on_grid_load=True)
+    
---
+++ pages/03_📊_analisedetalhada.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import plotly.express as px
+
+
+
+st.header("Análise Detalhada dos Dados")
+
+
+if 'dados' not in st.session_state:
+    st.error("Os dados não foram carregados")
+else:
+    dados = st.session_state['dados']
+    col1, col2 = st.columns(2)
+
+    with col1:
+        fig1 = px.histogram(dados, x='VALOREMPENHO', title='Histograma do Valor de Empenho')
+        st.plotly_chart(fig1, use_container_width=True)
+        
+        fig2 = px.box(dados, x='VALOREMPENHO', title='Boxplot do Valor de Empenho')
+        st.plotly_chart(fig2, use_container_width=True)
+
+
+    with col2:
+        
+        fig3 = px.histogram(dados, x='PIB', title='Histograma do Valor do PIB')
+        st.plotly_chart(fig3, use_container_width=True)
+        
+        fig4 = px.box(dados, x='PIB', title='Boxplot do Valor do PIB')
+        st.plotly_chart(fig4, use_container_width=True)
